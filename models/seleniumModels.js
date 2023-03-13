f20c899235a0bcfe419ea41d0617eab988995e86
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2023-03-13 18:39:50 +0000

    passing driver and passing body completed

--- models/seleniumModels.js
+++ models/seleniumModels.js
@@ -1,60 +1,28 @@
-const { By, Key, Builder } = require("selenium-webdriver");
+const { By, Key, Builder, until, WebDriver } = require("selenium-webdriver");
 require("chromedriver");
+const _http = require('selenium-webdriver/http');
 
+let google_driver = async function () {
+  driver = await new Builder().forBrowser("chrome").build();
+  driver.getSession().then(function (session) {
+        console.log("Session:" + session.getId());
+        sessionId = session.getId();
+      })
 
-async function test_case() {
-    let driver = await new Builder().forBrowser("chrome").build();
-  
-    await driver.get("https://google.com");
-      await driver.findElement(By.className("QS5gu sy4vM")).click();
-    await driver.findElement(By.name("q")).sendKeys("Hello, world", Key.RETURN);
-  
-    setInterval(function () {
-      driver.quit();
-    }, 5000);
-  }
+  await driver.get("https://google.com");
+  return driver;
+}
 
-  async function twitter_login() {
-    let driver = await new Builder().forBrowser("chrome").build();
-    await driver.get("https://twitter.com");
-      await driver.findElement(By.className("css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0")).click();
-    // await driver.findElement(By.name("q")).sendKeys("Hello, world", Key.RETURN);
-  
-    // setInterval(function () {
-    //   driver.quit();
-    // }, 5000);
-  }
-//   let driver;
-  async function twitter_login() {
-    let driver = await new Builder().forBrowser("chrome").build();
-  
-    await driver.get("https://twitter.com");
-    //   await driver.findElement(By.className("css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0")).click();
-    // await driver.findElement(By.name("q")).sendKeys("Hello, world", Key.RETURN);
-    // await driver.findElement(By.className("r-30o5oe r-1niwhzg r-17gur6a r-1yadl64 r-deolkf r-homxoj r-poiln3 r-7cikom r-1ny4l3l r-xyw6el r-y0fyvk r-1dz5y72 r-fdjqy7 r-13qz1uu")).click();
-    // await driver.findElement(By.className("r-30o5oe r-1niwhzg r-17gur6a r-1yadl64 r-deolkf r-homxoj r-poiln3 r-7cikom r-1ny4l3l r-xyw6el r-y0fyvk r-1dz5y72 r-fdjqy7 r-13qz1uu")).sendKeys("Hello, world", Key.RETURN);
-    // await twitter_loginCred(driver)
-    // setInterval(function () {
-    //   driver.quit();
-    // }, 5000);
-    return driver;
-  }
-  async function twitter_loginCred(driver) {
-    // let driver = await new Builder().forBrowser("chrome").build();
-    
-    // await driver.get("https://twitter.com");
-    await driver.findElement(By.className("r-30o5oe r-1niwhzg r-17gur6a r-1yadl64 r-deolkf r-homxoj r-poiln3 r-7cikom r-1ny4l3l r-xyw6el r-y0fyvk r-1dz5y72 r-fdjqy7 r-13qz1uu")).click();
-    await driver.findElement(By.className("r-30o5oe r-1niwhzg r-17gur6a r-1yadl64 r-deolkf r-homxoj r-poiln3 r-7cikom r-1ny4l3l r-xyw6el r-y0fyvk r-1dz5y72 r-fdjqy7 r-13qz1uu")).sendKeys("Hello, world", Key.RETURN);
-  
-    // setInterval(function () {
-    //   driver.quit();
-    // }, 5000);
-  }
+async function test_case(driver, bodyValue) {
+  await driver.get("https://google.com");
+  await driver.findElement(By.className("QS5gu sy4vM")).click();
+  await driver.findElement(By.name("q")).sendKeys(bodyValue.text, Key.RETURN);
+}
 
-
-  module.exports={
-    test_case,
-    twitter_login,
-    twitter_loginCred,
-    
-  }
+module.exports = {
+  test_case,
+  // test_google,
+  google_driver,
+  //twitter_login,
+  //twitter_loginCred,
+};
--- routes/seleniumRouter.js
+++ routes/seleniumRouter.js
@@ -2,32 +2,17 @@ const { By, Key, Builder } = require("selenium-webdriver");
 require("chromedriver");
 const express = require("express");
 const router = express();
-const {test_case, twitter_login, twitter_loginCred}=require("../models/seleniumModels");
+const {
+  test_case,
+  google_driver
+  //twitter_login,
+  //twitter_loginCred,
+} = require("../models/seleniumModels");
 // const driver = twitter_login();
 
-router.get("/selenium", async function(req, res) {
+router.get("/selenium", async function (req, res) {
   try {
-    const result = await test_case();
-    return res.json({ success: true, payload: result });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ success: false, error: error.message });
-  }
-});
-
-router.get("/twitterPage", async function(req, res) {
-  try {
-    const result = await twitter_login();
-    console.log(result);
-    return res.json({ success: true, payload: result });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ success: false, error: error.message });
-  }
-});
-router.get("/twitterLoginCred", async function(req, res) {
-  try {
-    const result = await twitter_loginCred(driver);
+    const result = await google_driver();
     return res.json({ success: true, payload: result });
   } catch (error) {
     console.error(error);
@@ -37,9 +22,30 @@ router.get("/twitterLoginCred", async function(req, res) {
 
 
 
+router.post("/selenium", async function(req,res) {
+  const result = await test_case(driver, req.body)
+  return res.json({success: true, payload: result}) 
+})
 
+// router.get("/twitterPage", async function(req, res) {
+//   try {
+//     const result = await twitter_login();
+//     console.log(result);
+//     return res.json({ success: true, payload: result });
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(500).json({ success: false, error: error.message });
+//   }
+// });
+// router.get("/twitterLoginCred", async function(req, res) {
+//   try {
+//     const result = await twitter_loginCred();
+//     return res.json({ success: true, payload: result });
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(500).json({ success: false, error: error.message });
+//   }
+// });
 
-
-
-module.exports = router ;
+module.exports = router;
 // test_case();
